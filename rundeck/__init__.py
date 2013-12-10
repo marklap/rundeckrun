4f653f5f3fb20f721255476c9c4bc6c1141da93a
marklap <marklap@gmail.com>
2013-12-09 22:29:58 -0500

    release 0.1.0

--- rundeck/__init__.py
+++ rundeck/__init__.py
@@ -7,5 +7,5 @@ __docformat__ = "restructuredtext en"
 :contact: rundeckrun@mindmind.com
 :copyright: Mark LaPerriere 2013
 """
-__VERSION__ = (0, 0, 2)
+__VERSION__ = (0, 1, 0)
 VERSION = '.'.join(map(str, __VERSION__))
