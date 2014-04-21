eae330efe1c42e196dd4ffdfd539a933e825fc12
Mark LaPerriere <mark.a.laperriere@disney.com>
2014-04-21 09:46:23 -0400

    rework @shlomosh patch to set verify in Session instance

--- rundeck/connection.py
+++ rundeck/connection.py
@@ -132,6 +132,7 @@ class RundeckConnectionTolerant(object):
             raise InvalidAuthentication('Must supply either api_token or usr and pwd')
 
         self.http = requests.Session()
+        self.http.verify = verify_cert
         if api_token is not None:
             self.http.headers['X-Rundeck-Auth-Token'] = api_token
         elif usr is not None and pwd is not None:
@@ -213,7 +214,7 @@ class RundeckConnectionTolerant(object):
         :rtype: requests.Response
         """
         return requests.request(
-            method, url, params=params, data=data, headers=headers, files=files, verify=self.verify_cert)
+            method, url, params=params, data=data, headers=headers, files=files)
 
 
 class RundeckConnection(RundeckConnectionTolerant):
